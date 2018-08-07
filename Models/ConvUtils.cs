a18b03681f9260a089272f9cfc781fe5d4c3018e
IGN <mcshe@DESKTOP-SQT517A>
2018-08-07 15:13:09 +0300

    Commit 46. Upgrade ConvUtils model

--- Models/ConvUtils.cs
+++ Models/ConvUtils.cs
@@ -88,6 +88,17 @@ namespace csmon.Models
             return $"{value.Integral}.{fraction.TrimEnd('0')}";
         }
 
+        public static string FormatAmount(TestApi.Amount value)
+        {
+            if (value.Fraction == 0) return $"{value.Integral}.0";
+
+            var fraction = value.Fraction.ToString();
+            while (fraction.Length < 18)
+                fraction = "0" + fraction;
+
+            return $"{value.Integral}.{fraction.TrimEnd('0')}";
+        }
+
         public static string FormatSrc(string code)
         {
             if (code.Contains(Environment.NewLine)) return code;
