4d9c732c2f6f5cb5edfd4b75af7ac92b111d798b
IGN <mcshe@DESKTOP-SQT517A>
2018-09-12 12:37:18 +0300

    Commit 222. Adding Settings model

---
+++ Models/Settings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace csmon.Models
+{
+    public static class Settings
+    {
+        public static bool AllowNegativeTime;
+        public static int UpdStatsPeriodSec;
+        public static int UpdNodesPeriodSec;
+        public static int NodesLivePeriodSec;
+        public static int SignalPort;
+        public static int TpsIntervalSec;
+
+        public static void Parse(IConfiguration config)
+        {
+            AllowNegativeTime = bool.Parse(config["AllowNegativeTime"]);
+            UpdStatsPeriodSec = int.Parse(config["UpdStatsPeriodSec"]);
+            UpdNodesPeriodSec = int.Parse(config["UpdNodesPeriodSec"]);
+            NodesLivePeriodSec = int.Parse(config["NodesLivePeriodSec"]);
+            SignalPort = int.Parse(config["SignalPort"]);
+            TpsIntervalSec = int.Parse(config["TpsIntervalSec"]);
+        }
+    }
+}
