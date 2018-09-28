d16e8fd4d82fcfc2a5b4f4895567037a9e19c52e
IGN <mcshe@DESKTOP-SQT517A>
2018-09-28 13:45:25 +0300

    Commit 284. Major edits
    Improving NodesServiceState class
    Edits in extracting node data from the database for each network
    Edits in updating the list of nodes from signal server

--- Models/Services/NodesService.cs
+++ Models/Services/NodesService.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using csmon.Models.Db;
-using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -16,18 +16,23 @@ namespace csmon.Models.Services
     {
         // Gets a list of blockchain network nodes by given network id
         NodesData GetNodes(string network);
-        
-        // Gets node info by id (ip address)
-        Node FindNode(string id);
     }
 
     // The service that communicates with signal servers and stores info about network nodes
     public class NodesService : IHostedService, IDisposable, INodesService
     {
+        // Data (list of nodes), prepared for each network
+        private class NodesServiceState
+        {
+            public List<NodeInfo> Nodes = new List<NodeInfo>();
+        }
+
         private readonly ILogger _logger; // For logging
-        public const int LiveTimeMinutes = 3; // The period of time, in which inactive node is considered as active
         private readonly int _period = Settings.UpdNodesPeriodSec * 1000; // Signal server interrogation period
         private Timer _timer; //  A timer
+
+        // Data (list of nodes), prepared for each network
+        private readonly Dictionary<string,NodesServiceState> _states = new Dictionary<string, NodesServiceState>();
 
         // Constructor, parameters are provided by service provider
         public NodesService(ILogger<NodesService> logger)
@@ -38,6 +43,10 @@ namespace csmon.Models.Services
         // Service start point
         public Task StartAsync(CancellationToken cancellationToken)
         {
+            // Create states for each network
+            foreach (var net in Network.Networks)
+                _states.Add(net.Id, new NodesServiceState());
+
             // Create the timer and complete
             _timer = new Timer(OnTimer, null, _period, 0);
             return Task.CompletedTask;
@@ -48,60 +57,110 @@ namespace csmon.Models.Services
         {
             foreach (var network in Network.Networks)
             {
-                for (var numOfTryes = 0; numOfTryes < 3; numOfTryes++)
+                if (network.RandomNodes)
                 {
+                    // if option 'RandomNodes' is on, get only nodes, stored in DB
+                    // This option is for debugging/testing purpose
                     try
                     {
-                        if(string.IsNullOrEmpty(network.SignalIp)) break;
-
-                        using (var client = ApiFab.CreateSignalApi(network.SignalIp))
+                        using (var db = ApiFab.GetDbContext())
                         {
-                            var result = client.GetActiveNodes();
-                            var nodes = result.Nodes.Distinct((n1, n2) => n1.Ip.Equals(n2.Ip)).ToArray();
-                            using (var db = ApiFab.GetDbContext())
-                            {
-                                var dbNodes = db.Nodes.ToArray();
-                                foreach (var serverNode in nodes)
-                                {
-                                    var ip = serverNode.Ip;
-                                    if (string.IsNullOrEmpty(ip)) continue;
-                                    var exnode = dbNodes.FirstOrDefault(n => n.Ip.Equals(ip));
-                                    if (exnode != null)
-                                    {
-                                        exnode.ModifyTime = DateTime.Now;
-                                        exnode.Version = serverNode.Version;
-                                        exnode.Platform = serverNode.Platform;
-                                        exnode.Size = result.Nodes.Count(n => n.Ip.Equals(ip));
-                                        db.Nodes.Update(exnode);
-                                    }
-                                    else
-                                    {
-                                        var uri = new Uri($"https://ipapi.co/{ip}/json/");
-                                        var nodestr = await GetAsync(uri);
-                                        var node = JsonConvert.DeserializeObject<Node>(nodestr);                                    
-                                        node.Ip = ip;
-                                        node.Version = serverNode.Version;
-                                        node.Network = network.Id;
-                                        node.Platform = serverNode.Platform;
-                                        node.Size = result.Nodes.Count(n => n.Ip.Equals(ip));
-                                        db.Nodes.Add(node);
-                                    }
-                                    db.SaveChanges();
-                                }
-                            }
-                        }
+                            // Get all nodes stored in db for this network
+                            var nodes = db.Nodes
+                                .Where(n => n.Network.Equals(network.Id))
+                                .Select(n => new NodeInfo(n))
+                                .ToList();
 
-                        break;
+                            // Put it here, where client page gets the data 
+                            _states[network.Id].Nodes = nodes;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        // ignored
+                    }
+                }
+                else // Otherwise, deal with the signal server
+                {
+                    // if no signal server configured, just skip this network
+                    if (string.IsNullOrEmpty(network.SignalIp)) continue;
+
+                    try
+                    {
+                        // Update the list of nodes from API of signal server
+                        await UpdateNetworkNodes(network);
                     }
                     catch (Exception e)
                     {
-                        _logger.LogError(e, $"Exception in NodesSource NumOfTryes={numOfTryes}");
-                        Thread.Sleep(1000);
+                        // In case of error (server unavailable), log this and clear the list
+                        _logger.LogError(e, "Exception in NodesSource");
+                        _states[network.Id].Nodes = new List<NodeInfo>();
                     }
                 }
             }
 
+            // Schedule next time
             _timer.Change(_period, 0);
+        }
+
+        // Updates the list of nodes, from signal server
+        private async Task UpdateNetworkNodes(Network network)
+        {
+            // Create a connection to the signal server API
+            using (var client = ApiFab.CreateSignalApi(network.SignalIp))
+            {
+                // Get the list of nodes from API
+                var result = client.GetActiveNodes();
+
+                // Convert nodes to nodeInfos
+                var nodes = result.Nodes.Select(n => new NodeInfo(n)).ToList();
+
+                // Try to get country and geo-location for all nodes
+                try
+                {
+                    // Connect to DB
+                    using (var db = ApiFab.GetDbContext())
+                    {
+                        // Get nodes, stored in db
+                        var dbNodes = db.Nodes.ToList();
+
+                        for (var i = 0; i < nodes.Count; i++)
+                        {                            
+                            var nodeInfo = nodes[i];
+
+                            // Try to find node in db by ip address
+                            var exnode = dbNodes.FirstOrDefault(n => n.Ip.Equals(result.Nodes[i].Ip));
+
+                            // If found, get stored country from it
+                            if (exnode != null)
+                            {
+                                nodeInfo.Country = exnode.Country;
+                                nodeInfo.CountryName = exnode.Country_name;
+                            }
+                            else // Otherwise, try to get info by ip using ipapi.co web service
+                            {
+                                var uri = new Uri("https://ipapi.co/" + $"{result.Nodes[i].Ip}/json/");
+                                var nodestr = await GetAsync(uri);
+                                var node = JsonConvert.DeserializeObject<Node>(nodestr);
+                                node.Ip = result.Nodes[i].Ip;
+
+                                // If no exceptions, store all data in db, 
+                                // because we don't want to use ipapi.co every time...
+                                db.Nodes.Add(node);
+                                db.SaveChanges();
+                                dbNodes.Add(node);
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+
+                // Put the list into the storage
+                _states[network.Id].Nodes = nodes;
+            }
         }
 
         // Makes Http request asynchronously 
@@ -115,33 +174,13 @@ namespace csmon.Models.Services
         // Gets the list of network nodes by network id
         public NodesData GetNodes(string network)
         {
-            // Get network by id
-            var net = Network.GetById(network);
-
-            // Create DB connection
-            using (var db = ApiFab.GetDbContext())
+            // Prepare the result and return
+            var result = new NodesData
             {
-                // Select nodes from DB by selected network
-                var now = DateTime.Now;
-                var nodes = db.Nodes.Where(n => n.Network == network &&
-                            (net.RandomNodes || n.ModifyTime.AddSeconds(Settings.NodesLivePeriodSec) >= now))
-                        .OrderBy(n => n.ModifyTime)
-                        .Take(1000).ToList();
-
-                // Prepare the result and return
-                var result = new NodesData { Nodes = nodes };
-                return result;
-            }
-        }
-
-        // Gets node by id (ip address)
-        public Node FindNode(string id)
-        {
-            // Create DB connection and try to find the node
-            using (var db = ApiFab.GetDbContext())
-            {
-                return db.Nodes.Find(id);
-            }
+                Nodes = _states[network].Nodes,
+                ShowKey = !Network.GetById(network).RandomNodes
+            };
+            return result;            
         }
 
         // Setvice stop point, called by the framework
