809e8295f022fba3ca534f6349a07537e9f2a76c
IGN <mcshe@DESKTOP-SQT517A>
2018-10-12 16:38:57 +0300

    Commit 332. Adding comments

--- Models/Db/CsmonDbContext.cs
+++ Models/Db/CsmonDbContext.cs
@@ -2,12 +2,25 @@
 
 namespace csmon.Models.Db
 {
+    /// <summary>
+    /// DB context for working with MS SQL database
+    /// </summary>
     public class CsmonDbContext : DbContext
     {
+        // Connection string
         public const string ConnectionString = "Data Source=localhost;Initial Catalog=csmon;Integrated Security=True";
+
+        // Table for nodes data (Country, Location, etc.)
         public DbSet<Node> Nodes { get; set; }
+
+        // A list of pre-defined smart contract addresses
         public DbSet<Smart> Smarts { get; set; }
+
+        // Table for storing network speed measurements, that used for displaying chart on the "Transactions per second" page
         public DbSet<Tp> Tps { get; set; }
+
+        //  A query for requesting TPS data, from the Tps page
+        public DbQuery<Point> Points { get; set; }
 
         public CsmonDbContext(DbContextOptions options)
             : base(options)
