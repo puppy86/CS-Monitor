398c5ad4258735eacb0e14005537d2194621e594
IGN <mcshe@DESKTOP-SQT517A>
2018-10-19 13:31:41 +0300

    Commit 342. Major edits
    Adding async methods for CsRequests views(Submit, ReCaptcha checking, SendEmail)

--- Controllers/ToolsController.cs
+++ Controllers/ToolsController.cs
@@ -1,5 +1,12 @@
-﻿using csmon.Models.Services;
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using csmon.Models;
+using csmon.Models.Services;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace csmon.Controllers
 {
@@ -16,7 +23,7 @@ namespace csmon.Controllers
         }
 
         public IActionResult Nodes()
-        {            
+        {
             return View();
         }
 
@@ -25,5 +32,60 @@ namespace csmon.Controllers
             return View(new GraphData());
         }
 
+        public IActionResult CSRequest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CsRequestSubmit(string address, string email, int amount)
+        {
+            // Check ReCaptcha
+            var jsonResponse = await GetRecaptchaResponseAsync(Request.Form["g-Recaptcha-Response"]);
+            dynamic jsonData = JObject.Parse(jsonResponse);
+
+            // If check fails return user back to form
+            if (jsonData.success != "true")
+                return View("CSRequest");            
+
+            // Send email to support
+            var msg = new StringBuilder();
+            msg.AppendLine($"A Request for CS has been submitted at {Request.Host}");
+            msg.AppendLine();
+            msg.AppendLine($"Network: {Network.GetById(RouteData.Values["network"].ToString()).Title}");
+            msg.AppendLine($"Address: {address}");
+            msg.AppendLine($"Email: {email}");
+            msg.AppendLine($"Amount: {amount} CS");
+            msg.AppendLine();
+            await SendEmailAsync(Config.EmailToAddress, "Request CS", msg.ToString());
+
+            // Show success message
+            return View("CsRequestConfirm");
+        }
+
+        public static async Task<string> GetRecaptchaResponseAsync(string reCaptchaResponse)
+        {
+            var uri = $"https://www.google.com/recaptcha/api/siteverify?secret={Config.RecaptchaKey}&response={reCaptchaResponse}";
+            var httpClient = new HttpClient();
+            return await httpClient.GetStringAsync(uri);
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient(Config.EmailFromHost, Config.EmailFromPort)
+            {
+                UseDefaultCredentials = false,
+                EnableSsl = true,
+                Credentials = new NetworkCredential(Config.EmailFromAddress, Config.EmailFromKey)
+            };
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(Config.EmailFromAddress)
+            };
+            mailMessage.To.Add(email);
+            mailMessage.Subject = subject;
+            mailMessage.Body = htmlMessage;
+            return client.SendMailAsync(mailMessage);
+        }
     }
 }
