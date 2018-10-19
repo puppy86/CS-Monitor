a22f32ce930097c06e29ec07bf686a90462c4584
IGN <mcshe@DESKTOP-SQT517A>
2018-10-19 13:23:18 +0300

    Commit 339. Adding a Config class

---
+++ Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csmon
+{
+    public static class Config
+    {
+        public const string ConnectionString;
+        public const string EmailFromHost;
+        public const int EmailFromPort;
+        public const string EmailFromAddress;
+        public const string EmailFromKey;
+        public const string EmailToAddress;
+        public const string RecaptchaKey;
+    }
+}
