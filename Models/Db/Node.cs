5ec6482b1d82f52f1306f02ea55e548e6d86f842
IGN <mcshe@DESKTOP-SQT517A>
2018-11-13 14:38:43 +0300

    Commit 441. Major edits and refactoring for Node data in DB

--- Models/Db/Node.cs
+++ Models/Db/Node.cs
@@ -8,19 +8,43 @@ namespace csmon.Models.Db
     public class Node
     {
         [Key]
-        [MaxLength(50)]
+        public string PublicKey { get; set; }
         public string Ip { get; set; }
         public string Network { get; set; }
+        public string Version { get; set; }
+        public byte Platform { get; set; }
+        public int CountTrust { get; set; }
+        public DateTime TimeRegistration { get; set; }
+        public long TimeActive { get; set; }
+        public bool Random { get; set; }
+
+        public Node()
+        {
+        }
+
+        public Node(NodeInfo node, string networkId) : this()
+        {
+            PublicKey = node.PublicKey;
+            Ip = node.Ip;
+            Network = networkId;
+            Platform = node.Platform;
+            Version = node.Version;
+            CountTrust = node.CountTrust;
+            TimeRegistration = node.TimeRegistration;
+            TimeActive = node.TimeActive;
+        }
+    }
+
+    public class Location
+    {
+        [Key]
+        public string Ip { get; set; }
         public float Latitude { get; set; }
         public float Longitude { get; set; }
-        public DateTime ModifyTime { get; set; } = DateTime.Now;
         public string City { get; set; }
         public string Region { get; set; }
         public string Country_name { get; set; }
         public string Org { get; set; }
-        public string Version { get; set; }
-        public string Platform { get; set; }
-        public int Size { get; set; }
         public string Country { get; set; }
     }
 }
